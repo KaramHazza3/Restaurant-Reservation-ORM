79f32154a6c3015ffec431c6ff3a1fa75d502710
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-10-23 15:38:40 +0300

    Add exceptions

---
+++ RestaurantReservation/Exceptions/NoAvailableTablesException.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantReservation.Exceptions;
+
+[Serializable]
+public class NoAvailableTablesException : Exception
+{
+    public NoAvailableTablesException()
+    {
+    }
+
+    public NoAvailableTablesException(string? message) : base(message)
+    {
+    }
+}
---
+++ RestaurantReservation/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantReservation.Exceptions;
+
+[Serializable]
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string? message) : base(message)
+    {
+    }
+}
